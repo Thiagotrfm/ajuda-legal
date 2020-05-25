35e255290a430c94e97a9b837d32d8545a58d1f7
thiagorfm <t_moreira@outlook.com>
2020-05-25 14:12:06 -0300

    chore(init): starter project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import TopBar from './components/TopBar';
+
+import 'antd/dist/antd.css';
+
+const { Content, Header } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Content>
+        <TopBar />
+        <Router>
+          <Switch>
+              <Route path="/">
+                <Home/>
+              </Route>
+            </Switch>
+        </Router>
+      </Content>
+    </Layout>
   );
 }
 
---
+++ src/components/TopBar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.less';
+
+const componentName = 'top-bar';
+
+function TopBar() {
+  return (
+      <div className={componentName}>
+          Olá!
+      </div>
+  );
+}
+
+export default TopBar;
